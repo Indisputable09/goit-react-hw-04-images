cc6eb18ab234636bb35b6d5522a2cd3f4dd3f1b8
Indisputable09 <sainchyksasha2015999@gmail.com>
2022-09-08 17:07:55 +0300

    Removed commented lines

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,45 +1,40 @@
-import { useEffect } from "react";
-import { createPortal } from "react-dom";
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
 
-const Modal = ({onClose, children}) => {
-    useEffect(() => {
-        const handleKeyDown = e => {
-        if (e.code === 'Escape') {
-            onClose();
-        }
+const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
     };
-        window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keydown', handleKeyDown);
 
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        }
-    }, [onClose]);
-
-    // const handleKeyDown = e => {
-    //     if (e.code === 'Escape') {
-    //         onClose();
-    //     }
-    // };
-
-    const handleBackDropClick = e => {
-        if (e.currentTarget === e.target) {
-            onClose();
-        }
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
     };
+  }, [onClose]);
 
-    return createPortal(<div className="Overlay" onClick={handleBackDropClick}>
-            <div className="Modal">
-                {children}
-            </div>
-        </div>, modalRoot)
-}
+  const handleBackDropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div className="Overlay" onClick={handleBackDropClick}>
+      <div className="Modal">{children}</div>
+    </div>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
-    onClose: PropTypes.func.isRequired,
-    children: PropTypes.object.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.object.isRequired,
 };
 
 export default Modal;
